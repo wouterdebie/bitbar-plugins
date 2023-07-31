77f743f3c95b51507044453365e9d20ccf041d4f
Wouter de Bie <wouter.de.bie@datadoghq.com>
2023-07-31 13:36:15 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use anyhow::Error;
 use anyhow::Result;
 use chrono::prelude::*;
 use chrono::Utc;
-use chrono_tz::US::Eastern;
 use prettytable::cell;
 use prettytable::format;
 use prettytable::row;
@@ -10,8 +9,9 @@ use prettytable::Table;
 
 const API_URL: &str = "https://api.wsj.net/api/dylan/quotes/v2/comp/quoteByDialect?dialect=official&needed=CompositeTrading|TimeZoneInfo&MaxInstrumentMatches=1&accept=application/json&EntitlementToken=cecc4267a0194af89ca343805a3e57af&ckey=cecc4267a0&dialects=Charting&id=";
 
-const STOCKS: [&str; 13] = [
+const STOCKS: [&str; 14] = [
     "Stock-US-DDOG",
+    "Stock-US-GRND",
     "Stock-US-RDHL",
     "Stock-US-SPOT",
     "Stock-US-BMRA",
@@ -33,8 +33,7 @@ const TOP_GREEN: &str = "#2CE035";
 const TOP_RED: &str = "#FF6565";
 
 fn main() -> Result<(), Error> {
-    let now = Utc::now().with_timezone(&Eastern);
-    let current_time = now.time();
+    let current_time = Local::now().time();
 
     let (top_green, top_red) = if current_time >= NaiveTime::from_hms_opt(6, 0, 0).unwrap()
         && current_time < NaiveTime::from_hms_opt(8, 00, 0).unwrap()
@@ -60,9 +59,11 @@ fn main() -> Result<(), Error> {
         let matches = &stock["Matches"][0];
         let ct = &matches["CompositeTrading"];
         let ticker = matches["Instrument"]["Ticker"].as_str().unwrap();
-        let last_price = &ct["Last"].as_object().unwrap()["Price"]["Value"];
-        let high = &ct["High"]["Value"];
-        let low = &ct["Low"]["Value"];
+        let last_price = &ct["Last"].as_object().unwrap()["Price"]["Value"]
+            .as_f64()
+            .unwrap();
+        let high = &ct["High"]["Value"].as_f64().unwrap();
+        let low = &ct["Low"]["Value"].as_f64().unwrap();
         let change_value = ct["NetChange"]["Value"].as_f64().unwrap();
         let pct = ct["ChangePercent"].as_f64().unwrap();
         let stype = matches["Instrument"].as_object().unwrap()["Types"][0]["Name"]
@@ -84,32 +85,27 @@ fn main() -> Result<(), Error> {
         let naive_dt = DateTime::parse_from_rfc3339(&last_trade_with_offset).unwrap();
         let utc_dt = naive_dt.with_timezone(&Utc);
 
-        // Check if the last trade was within the last minute
-        let trading = Utc::now() - utc_dt < chrono::Duration::minutes(1);
-        let mut row = if trading { row![" "] } else { row!["☾"] };
+        // Check if the last trade was within the last 10 minutes
+        let trading = Utc::now() - utc_dt < chrono::Duration::minutes(10);
 
+        let mut row = if trading { row![" "] } else { row!["☾"] };
         row.add_cell(cell!(ticker));
         row.add_cell(cell!(r->format!("{:.2}", last_price)));
         row.add_cell(cell!(symbol));
-
-        if change_value >= 0.0 {
-            row.add_cell(cell!(r->format!("+{:.2}", change_value)));
-            row.add_cell(cell!(r->format!("+{:.2}%", pct)));
-        } else {
-            row.add_cell(cell!(r->format!("{:.2}", change_value)));
-            row.add_cell(cell!(r->format!("{:.2}%", pct)));
-        };
-
+        row.add_cell(cell!(r->format!("{:+.2}", change_value)));
+        row.add_cell(cell!(r->format!("{:+.2}%", pct)));
         row.add_cell(cell!(format!("({:.2} - {:.2})", low, high)));
         row.add_cell(cell!("|"));
         row.add_cell(cell!(format!("size={}", FONT_SIZE)));
 
+        // Duplicate the first row for the top row , but don't set the font
         if i == 0 {
-            row.add_cell(cell!(format!("color={}", top_color)));
-            table.add_row(row.clone());
+            let mut top_row = row.clone();
+            top_row.add_cell(cell!(format!("color={}", top_color)));
+            table.add_row(top_row);
             table.add_row(row![H2->"---"]);
-            row.remove_cell(row.len() - 1);
         }
+
         row.add_cell(cell!(format!("font='Hack Nerd Font'")));
         row.add_cell(cell!(format!("color={}", color)));
         row.add_cell(cell!(format!(
